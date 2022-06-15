41b0eb9f1c90c591793448fe3d3af3fbf44ec317
mankic <miic0803@gmail.com>
2022-06-16 01:24:38 +0900

    수정 - kwargs 예시

--- kwargs.py
+++ kwargs.py
@@ -1,4 +1,13 @@
-# 
-# def my_function(**kwargs):
-#     for key, value in kwargs.items():   # 딕셔너리를 리스트처럼 풀수있는 함수 
+# 입력받은 정보 출력하기
+
+def user_info(**kwargs):
+    print(kwargs.values())
+    print(kwargs['name'])
+    print(f'이름:{kwargs["name"]} / 나이:{kwargs["age"]}')
+    return
+
+user_info(name='홍길동', age='47')
+
+# def user_info(**kwargs):
+#     for key, value in kwargs.items():   # 딕셔너리를 리스트처럼 풀수있는 기능 .items()
 #         print()
